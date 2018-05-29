9469c2a2b5fb9df326741318904ec4b88675cc04
Herivelto Junior <herijr@gmail.com>
2018-05-29 19:24:10 -0300

    criando a classe Ball

--- Animation example.py
+++ Animation example.py
@@ -5,27 +5,29 @@ import time
 WIDTH = 800
 HEIGHT = 600
 
-
 tk = Tk()
-
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
-tk.title("Drawing")
-
+tk.title("Boucing Ball")
 canvas.pack()
 
-ball = canvas.create_oval(10, 10, 60, 60, fill="orange")
+class Ball:
+    def __init__(self):
+        self.shape = canvas.create_oval(10, 10, 60, 60, fill="orange")
+        self.xspeed = 4
+        self.yspeed = 5
 
-xspeed = 4
-yspeed = 5
+    def move(self):
+        canvas.move(self.shape, self.xspeed, self.yspeed)
+        pos = canvas.coords(self.shape)
+        if pos[3] >= HEIGHT or pos[1] <= 0:
+            self.yspeed = -self.yspeed
+        if pos[2] >= WIDTH or pos[0] <= 0:
+            self.xspeed = -self.xspeed
+newball = Ball()
 
 while True:
-    canvas.move(ball, xspeed, yspeed)
-    pos = canvas.coords(ball)
-    if pos[3] >= HEIGHT or pos[1] <= 0:
-        yspeed = -yspeed
-    if pos[2] >= WIDTH or pos[0] <= 0:
-        xspeed = -xspeed
+    newball.move()
     tk.update()
     time.sleep(0.01)
     
-canvas.mainloop()
+tk.mainloop()
