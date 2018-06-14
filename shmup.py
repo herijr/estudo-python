367374eb827f36d546cd193065248b2361c3cdc4
Herivelto Junior <herijr@gmail.com>
2018-06-14 15:28:12 -0300

    Pontuação e texto na tela

--- shmup.py
+++ shmup.py
@@ -26,6 +26,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
 class Player(pygame.sprite.Sprite):
     # sprite for the player
     def __init__(self):
@@ -134,7 +142,7 @@ for i in range(8):
     all_sprites.add(m)
     mobs.add(m)
 
-
+score = 0
 # Game loop
 running = True
 while running:
@@ -153,6 +161,7 @@ while running:
     # check to see if a bullet hit a mob
     hits = pygame.sprite.groupcollide(mobs,bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
@@ -166,6 +175,7 @@ while running:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
     # *after* drawing everything, flip the display
     pygame.display.flip()
 
